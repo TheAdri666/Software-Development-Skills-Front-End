eb84cda83ad3d3207cddfc9bd56903a634d9ce8a
TheAdri666 <adrianinfante66@gmail.com>
2023-07-10 18:59:38 +0200

    Added grid. Fixed overflow issue

--- Final-Project/dist/js/main.js
+++ Final-Project/dist/js/main.js
@@ -1,6 +1,7 @@
 // DOM Elements
 const menuBtn = document.querySelector('.menu-btn');
 const menuNav = document.querySelector('.menu-nav');
+const body = document.querySelector('body');
 
 // Menu State
 let showMenu = false;
@@ -11,10 +12,12 @@ function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add('close');
     menuNav.classList.remove('hide');
+    body.classList.add('hide-overflow');
     showMenu = true;
   } else {
     menuBtn.classList.remove('close');
     menuNav.classList.add('hide');
+    body.classList.remove('hide-overflow');
     showMenu = false;
   }
 }
